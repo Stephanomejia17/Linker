884e8dcde0831cbdaf5fa9f24c08568270c5e411
unknown <nathaliav8194@gmail.com>
2025-09-21 12:21:51 -0500

    Signup usuarios y empresas

---
+++ Frontend/src/app/features/signup-empresas/signup-empresas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-empresas',
+  imports: [],
+  templateUrl: './signup-empresas.html',
+  styleUrl: './signup-empresas.css'
+})
+export class SignupEmpresas {
+
+}
