a4c9a189f7bda652d3ac79cfd99654132fe220db
Stephanomejia17 <stephano.mejia@outlook.es>
2025-10-25 22:30:06 -0500

    Inicializador de bases de datos

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,13 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DataSource } from 'typeorm';
 
 async function bootstrap() {
-const app = await NestFactory.create(AppModule);
-app.enableCors({
-  origin: 'http://localhost:4200', // El puerto angular
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  credentials: true,
-});
-await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200', // El puerto angular
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+  });
+
+  const postgresDataSource = app.get<DataSource>(
+    'postgresConnectionDataSource',
+  );
+  try {
+    if (!postgresDataSource.isInitialized) {
+      await postgresDataSource.initialize();
+      console.log('Conectado a la base de datos principal (Supabase)');
+    }
+  } catch (err) {
+    console.error('Error conectando a Supabase:', err.message);
+  }
+
+  try {
+    const oracleDataSource = app.get<DataSource>('oracleConnectionDataSource');
+    if (!oracleDataSource.isInitialized) {
+      await oracleDataSource.initialize();
+    }
+
+    const result = await oracleDataSource.query('SELECT * FROM DUAL');
+    console.log('Conectado a Oracle correctamente:', result);
+  } catch (err) {
+    console.error('Error conectando a Oracle:', err.message);
+  }
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
